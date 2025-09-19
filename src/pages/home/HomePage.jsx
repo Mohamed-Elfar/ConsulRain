946b40e9f2cebd20215377f9dec447d69c78d88c
Mohamed-Elfar <st2020048@kfs-hiet.edu.eg>
2025-09-19 17:01:44 +0300

    added lazy loading to home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,23 +1,63 @@
-import HeroSection from "../../components/homeComponents/HeroSection";
-import React from "react";
-import { EmpowerSection, StatsSection } from "../../components";
-import Founders from "../../components/foundersSection/Founders";
-import OurPartners from "../../components/PartnersSection/OurPartners";
-import Services from "../../components/OurServices/OurServices";
-import VisionMission from "../../components/VisionMission/VisionMission";
-import IntegratedSolutions from "../../components/IntegratedSolutions/IntegratedSolutions";
+// import HeroSection from "../../components/homeComponents/HeroSection";
+// import React from "react";
+// import { EmpowerSection, StatsSection } from "../../components";
+// import Founders from "../../components/foundersSection/Founders";
+// import OurPartners from "../../components/PartnersSection/OurPartners";
+// import Services from "../../components/OurServices/OurServices";
+// import VisionMission from "../../components/VisionMission/VisionMission";
+// import IntegratedSolutions from "../../components/IntegratedSolutions/IntegratedSolutions";
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <HeroSection />
+//       <IntegratedSolutions />
+//       <VisionMission />
+//       <StatsSection />
+//       <EmpowerSection />
+//       <Services />
+//       <Founders />
+//       <OurPartners />
+//     </div>
+//   );
+// }
+import React, { Suspense, lazy } from "react";
+const HeroSection = lazy(() =>
+  import("../../components/homeComponents/HeroSection")
+);
+const IntegratedSolutions = lazy(() =>
+  import("../../components/IntegratedSolutions/IntegratedSolutions")
+);
+const VisionMission = lazy(() =>
+  import("../../components/VisionMission/VisionMission")
+);
+const StatsSection = lazy(() =>
+  import("../../components/StatsSection/StatsSection")
+);
+const EmpowerSection = lazy(() =>
+  import("../../components/EmpowerSection/EmpowerSection")
+);
+const Services = lazy(() => import("../../components/OurServices/OurServices"));
+const Founders = lazy(() =>
+  import("../../components/foundersSection/Founders")
+);
+const OurPartners = lazy(() =>
+  import("../../components/PartnersSection/OurPartners")
+);
 
 export default function HomePage() {
   return (
-    <div>
-      <HeroSection /> 
-      <IntegratedSolutions />
-      <VisionMission />
-      <StatsSection />
-      <EmpowerSection />
-      <Services />
-      <Founders />
-      <OurPartners />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <HeroSection />
+        <IntegratedSolutions />
+        <VisionMission />
+        <StatsSection />
+        <EmpowerSection />
+        <Services />
+        <Founders />
+        <OurPartners />
+      </div>
+    </Suspense>
   );
 }
